4a024b690dabf456acd1adf4abea6ebcd3c2540f
Barry <barryrichards05@gmail.com>
2026-03-02 13:22:59 +0000

    added tests for LandingPage and Staff

---
+++ src/test/java/com/makers/makersbnb/StaffTest.java
@@ -0,0 +1,42 @@
+package com.makers.makersbnb;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class StaffTest {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    public void teamIsReturned() {
+        page.navigate("http://localhost:8080/team");
+        Locator pageBody = page.locator("body");
+        assertThat(pageBody).containsText("Toby, Katerina, Sandy");
+    }
+}
