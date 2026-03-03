4501d1e1a1951180264baa4e0e8f1f443ffa06d4
Barry <barryrichards05@gmail.com>
2026-03-03 12:31:07 +0000

    implemented SpaceRepository

--- src/main/java/com/makers/makersbnb/controller/StaticPageController.java
+++ src/main/java/com/makers/makersbnb/controller/StaticPageController.java
@@ -1,16 +1,22 @@
 package com.makers.makersbnb.controller;
 
+import com.makers.makersbnb.repository.SpaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @RestController
 public class StaticPageController {
+
+    @Autowired
+    SpaceRepository spaceRepository;
 
     @GetMapping("/")
     public ModelAndView landingPage() {
         ModelAndView modelAndView = new ModelAndView("/LandingPage");
-        Integer nSpaces = 100;
+        long nSpaces = spaceRepository.count();
         Integer nBookings = 123;
         modelAndView.addObject("nSpaces", nSpaces);
         modelAndView.addObject("nBookings", nBookings);
---
+++ src/main/java/com/makers/makersbnb/repository/SpaceRepository.java
@@ -0,0 +1,10 @@
+package com.makers.makersbnb.repository;
+
+import java.util.List;
+import com.makers.makersbnb.model.Space;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpaceRepository extends CrudRepository<Space, Long> {
+
+    public List<Space> findSpaceByName(String name);
+}
