6b0855ebbf6b9565af5c97aa9b517a233decb262
Barry <barryrichards05@gmail.com>
2026-03-03 14:37:57 +0000

    Added new space form and implemented GET and POST mapping

--- src/main/java/com/makers/makersbnb/controller/SpaceController.java
+++ src/main/java/com/makers/makersbnb/controller/SpaceController.java
@@ -4,8 +4,10 @@ import com.makers.makersbnb.model.Space;
 import com.makers.makersbnb.repository.SpaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class SpaceController {
@@ -19,5 +21,19 @@ public class SpaceController {
         Iterable<Space> currentSpaces = spaceRepository.findAll();
         spacesView.addObject("currentSpaces", currentSpaces);
         return spacesView;
+    }
+
+    @GetMapping("/spaces/new")
+    public ModelAndView newSpaceForm() {
+        Space space = new Space();
+        ModelAndView newSpaceFormView = new ModelAndView("spaces/New");
+        newSpaceFormView.addObject("space", space);
+        return newSpaceFormView;
+    }
+
+    @PostMapping("/spaces")
+    public RedirectView createSpace(Space space) {
+        spaceRepository.save(space);
+        return new RedirectView("/spaces");
     }
 }
