6df49c817b7027afe048b64f237f6b08cbf12c60
Barry <barryrichards05@gmail.com>
2026-03-04 16:26:24 +0000

    added /spaces to permitAll

--- src/main/java/com/makers/makersbnb/config/SecurityConfiguration.java
+++ src/main/java/com/makers/makersbnb/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.makers.makersbnb.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,6 +28,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/spaces").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(withDefaults())
